2576d35c485761c5944dd213428fd657ef0df155
“kyleisme14” <kylekallman@gmail.com>
2022-10-12 17:31:55 -0700

    set up

---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    return (
+    <>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <Link to="/" className="navbar-logo">
+                    KYLE <ion-icon name="happy-outline"></ion-icon>
+                </Link>\
+
+                <div className='menu-icon' onClick={handleClick}>
+                    <ion-icon name={click ? "skull-outline" : "menu-outline"}></ion-icon>
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                            Services
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                            Products
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </>    
+  )
+}
+
+export default Navbar;
