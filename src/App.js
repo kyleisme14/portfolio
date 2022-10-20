3f3e08dd0a04196952101256bbb4f55767d3b6a9
“kyleisme14” <kylekallman@gmail.com>
2022-10-19 21:07:13 -0700

    added cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home'
 
 function App() {
   return (
     <>
-    <Router>
+    <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/' exact />
+        <Route path='/' element= {<Home />}/>
       </Routes>
-    </Router>
+    </BrowserRouter>
     </>
   );
 }
