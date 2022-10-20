c443e004a8719d87a647c3235919875796508448
“kyleisme14” <kylekallman@gmail.com>
2022-10-19 21:58:23 -0700

    pages

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,21 +1,64 @@
-import React from 'react'
-import CardItem from './CardItem'
-import './Cards.css'
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
 
 function Cards() {
   return (
     <div className='cards'>
-        <h1> Check out these Epic Destinations!</h1>
-        <div className='cards_container'>
-            <div className='cards_wrapper'>
-                <ul className='cards_items'>
-                    <CardItem />
-
-                </ul>
-            </div>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='/photos/blueclwon.png'
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/services'
+            />
+           
+            <CardItem
+              src='/photos/pillars.webp'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+            <CardItem
+              src='/photos/fakespace.png'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='/photos/blueclwon.png'
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/services'
+            />
+            <CardItem
+              src='/photos/yellowpants.png'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+            <CardItem
+              src='/photos/pillars.webp'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+            <CardItem
+              src='/photos/fakespace.png'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
